816905e128614fa127838d1f4e9d0e43670d2300
GitHub Action <action@github.com>
2021-08-01 07:37:15 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/flyingyuz/ideastringsgetter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.flyingyuz.ideastringsgetter.services
+
+import com.github.flyingyuz.ideastringsgetter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
